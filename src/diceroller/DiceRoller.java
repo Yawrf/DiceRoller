dc305123d3aa175208bdd617919100279320b5ec
Yawrf <rewilloughby@ymail.com>
2019-04-10 21:18:03 -0400

    Preparation for Parent
    A minor Tweak to integrate it into the Parent App

--- src/diceroller/DiceRoller.java
+++ src/diceroller/DiceRoller.java
@@ -17,15 +17,20 @@ import javafx.stage.Stage;
  */
 public class DiceRoller extends Application {
     
+    private static Scene scene = null;
+    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
         
         stage.setTitle("Dice Roller by Yawrf");
         stage.setScene(scene);
         stage.show();
+    }
+    
+    public static Scene getScene() {
+        return scene;
     }
 
     /**
